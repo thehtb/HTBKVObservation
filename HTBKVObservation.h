5fe9f30f3fbc7235ea32fb9844bf980f3b5ada88
Mark Aufflick <mark@aufflick.com>
2015-02-20 10:21:35 +1100

    Added some basic header docs

--- HTBKVObservation.h
+++ HTBKVObservation.h
@@ -8,35 +8,52 @@
 
 #import <Foundation/Foundation.h>
 
+/// Conveniently and safely use KVO, modelling KVO each observation as an object.
 @interface HTBKVObservation : NSObject
 
+/// The object that is the target of the observation
 @property (nonatomic, weak) id observedObject;
+
+/// The block that will be called whenver an observation fires
 @property (nonatomic, copy) void (^callbackBlock)(HTBKVObservation * observation, NSDictionary * changeDictionary);
+
+/// The keypath of the observedObject that is being observed
 @property (nonatomic, copy) NSString * keyPath;
+
+/// KVO options for the observation
 @property NSKeyValueObservingOptions options;
+
+/// False if the observation has been invalidated (either manually or because the target object was deallocated)
 @property (nonatomic, readonly) BOOL isValid;
 
+/// Returns a started observation
 + (HTBKVObservation *)observe:(id)observedObject
                       keyPath:(NSString *)keyPath
                       options:(NSKeyValueObservingOptions)options
                      callback:(void (^)(HTBKVObservation * observation, NSDictionary * changeDictionary))callbackBlock;
 
+/// Returns an array of started observations, each targetted to the same object for a range of key paths.
 + (NSArray *)observe:(id)observedObject
  forMultipleKeyPaths:(NSArray *)keyPaths
              options:(NSKeyValueObservingOptions)options
             callback:(void (^)(HTBKVObservation * observation, NSDictionary * changeDictionary))callbackBlock;
 
+/// Returns an observation that will automatically update the `boundObjectKeyPath` on the `boundObject` whenever the `observedKeyPath` changes on the `observedObject`
 + (HTBKVObservation *)bind:(id)observedObject
                    keyPath:(NSString *)observedKeyPath
                   toObject:(id)boundObject
                    keyPath:(NSString *)boundObjectKeyPath;
 
+/// Returns a pair of observations that will automatically keep the keypaths on the respective objects in sync. Care is taken not to enter an infinite loop :)
 + (NSArray *)bidirectionallyBind:(id)objectA
                          keyPath:(NSString *)objectAKeyPath
                       withObject:(id)objectB
                          keyPath:(NSString *)objectBKeyPath;
 
-- (BOOL)observe; // only necessary if you alloc/init yourself
-- (void)invalidate; // not necessary if the observation object lifecycle/dealloc will go away at the appropriate time
+/// Start the observation (only necessary if you alloc/init yourself)
+- (BOOL)observe;
+
+/// Stop the observation (not noramlly necessary if the observation object lifecycle/dealloc will go away at the appropriate time)
+- (void)invalidate;
 
 @end
